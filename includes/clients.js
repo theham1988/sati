99296e372d0957995fcf80f2810962bddaf6650c
Hamza atouani <hamza@satidigitalmedia.com>
2025-06-03 06:07:31 +0000

    Initial working version of dynamic GA4 models for all clients

---
+++ includes/clients.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    name: "Barracuda_Phuket",
+    source_dataset: "analytics_485512127",
+    output_schema: "barracuda_phuket_flattened"
+  },
+  {
+    name: "Island_Escape",
+    source_dataset: "analytics_427190025",
+    output_schema: "island_escape_flattened"
+  },
+  {
+    name: "Roost_Glamping",
+    source_dataset: "analytics_399579048",
+    output_schema: "roost_glamping_flattened"
+  }
+];
