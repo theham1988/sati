df800705f6bb5e7d1f0e86f7daa92c7c495a35b6
Hamza atouani <hamza@satidigitalmedia.com>
2025-06-03 06:54:21 +0000

    Add user view with user-level engagement and device aggregations

--- includes/clients.js
+++ includes/clients.js
@@ -2,16 +2,16 @@ module.exports = [
   {
     name: "barracuda_phuket",
     source_dataset: "analytics_485512127",
-    output_schema: "barracuda_phuket_flattened"
+    output_schema: "barracuda_phuket"
   },
   {
     name: "island_escape",
     source_dataset: "analytics_427190025",
-    output_schema: "island_escape_flattened"
+    output_schema: "island_escape"
   },
   {
     name: "roost_glamping",
     source_dataset: "analytics_399579048",
-    output_schema: "roost_glamping_flattened"
+    output_schema: "roost_glamping"
   }
 ];
