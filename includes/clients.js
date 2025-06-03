d2841ae78b4d0b0ad1a54b1b75a2bebc3df73cd8
Hamza atouani <hamza@satidigitalmedia.com>
2025-06-03 07:00:27 +0000

    sss

--- includes/clients.js
+++ includes/clients.js
@@ -1,16 +1,19 @@
 module.exports = [
   {
     name: "barracuda_phuket",
+    project_id: "sublime-elixir-458201-r4",
     source_dataset: "analytics_485512127",
     output_schema: "barracuda_phuket"
   },
   {
     name: "island_escape",
+    project_id: "sublime-elixir-458201-r4",
     source_dataset: "analytics_427190025",
     output_schema: "island_escape"
   },
   {
     name: "roost_glamping",
+    project_id: "sublime-elixir-458201-r4",
     source_dataset: "analytics_399579048",
     output_schema: "roost_glamping"
   }
