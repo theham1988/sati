b2364ead1e937205b05d1e8a8faf170ee08a5653
Hamza atouani <hamza@satidigitalmedia.com>
2025-06-03 07:19:59 +0000

    js

--- definitions/user.js
+++ definitions/user.js
@@ -1,20 +1,18 @@
+const clients = require("includes/clients.js");
 
-  const clients = require("includes/clients.js");
-
-  for (const client of clients) {
-    publish(`${client.name}_user`, {
-      type: "view",
-      schema: client.output_schema
-    }).query(ctx => `
-      SELECT
-        user_pseudo_id,
-        MAX(user_first_touch_timestamp) AS first_touch,
-        COUNT(DISTINCT event_name) AS event_count,
-        COUNT(DISTINCT event_date) AS active_days,
-        ARRAY_AGG(DISTINCT device.category)[OFFSET(0)] AS device,
-        ARRAY_AGG(DISTINCT geo.country)[OFFSET(0)] AS country
-      FROM \`${ctx.project}.${client.source_dataset}.events_*\`
-      GROUP BY user_pseudo_id
-    `);
-  }
-
+for (const client of clients) {
+  publish(`${client.name}_user`, {
+    type: "view",
+    schema: client.output_schema,
+  }).query(ctx => `
+    SELECT
+      user_pseudo_id,
+      MAX(user.first_touch_timestamp) AS first_touch,
+      COUNT(DISTINCT event_name) AS event_count,
+      COUNT(DISTINCT event_date) AS active_days,
+      ARRAY_AGG(DISTINCT device.category)[OFFSET(0)] AS device,
+      ARRAY_AGG(DISTINCT geo.country)[OFFSET(0)] AS country
+    FROM \`${client.project_id}.${client.source_dataset}.events_*\`
+    GROUP BY user_pseudo_id
+  `);
+}
