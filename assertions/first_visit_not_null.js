bcd65901b86a839b41681742e4159312cf401b51
Hamza atouani <hamza@satidigitalmedia.com>
2025-06-03 08:25:16 +0000

    js

--- assertions/first_visit_not_null.js
+++ assertions/first_visit_not_null.js
@@ -1,7 +1,7 @@
 const clients = require("includes/clients.js");
 
 for (const client of clients) {
-  assert(`${client.name}_first_visit_not_null`, `
+    assert(`${client.name}_first_visit_not_null`, `
     SELECT *
     FROM \`${client.project_id}.${client.output_schema}.${client.name}_user\`
     WHERE first_visit IS NULL
