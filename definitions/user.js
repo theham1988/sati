8047f498e16a8bf8c7883401f77bfc2e90ad5cf5
Hamza atouani <hamza@satidigitalmedia.com>
2025-06-04 04:37:18 +0000

    adding shared_metrics to user

--- definitions/user.js
+++ definitions/user.js
@@ -1,4 +1,5 @@
 const clients = require("includes/clients.js");
+const metrics = require("includes/shared_metrics/metrics.js");
 
 for (const client of clients) {
   publish(`${client.name}_user`, {
@@ -7,11 +8,11 @@ for (const client of clients) {
   }).query(ctx => `
     SELECT
       user_pseudo_id,
-      MIN(IF(event_name = 'first_visit', event_timestamp, NULL)) AS first_visit,
+      MIN(event_timestamp) FILTER (WHERE event_name = 'first_visit') AS first_visit,
       COUNT(DISTINCT event_name) AS event_count,
       COUNT(DISTINCT event_date) AS active_days,
-      ARRAY_AGG(DISTINCT device.category)[OFFSET(0)] AS device,
-      ARRAY_AGG(DISTINCT geo.country)[OFFSET(0)] AS country
+      ${metrics.device_category} AS device,
+      ${metrics.country} AS country
     FROM \`${client.project_id}.${client.source_dataset}.events_*\`
     GROUP BY user_pseudo_id
   `);
