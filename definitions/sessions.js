0e8c1f8c46d14775bd5fcc964091a182bcc49947
Hamza atouani <hamza@satidigitalmedia.com>
2025-06-04 04:43:31 +0000

    shared_metrics

--- definitions/sessions.js
+++ definitions/sessions.js
@@ -1,4 +1,5 @@
 const clients = require("includes/clients.js");
+const metrics = require("includes/shared_metrics/metrics.js");
 
 for (const client of clients) {
   publish(`${client.name}_sessions`, {
@@ -6,18 +7,16 @@ for (const client of clients) {
     schema: client.output_schema,
   }).query(ctx => `
     SELECT
+      session_id,
       user_pseudo_id,
-      event_bundle_sequence_id,
-      event_timestamp,
-      traffic_source.source AS source,
-      traffic_source.medium AS medium,
-      traffic_source.name AS campaign,
-      device.category AS device_category,
-      geo.country,
-      geo.region,
-      event_date,
-      (SELECT value.string_value FROM UNNEST(event_params) WHERE key = "page_location") AS page_location
+      MIN(event_timestamp) AS session_start,
+      MAX(event_timestamp) - MIN(event_timestamp) AS session_duration,
+      COUNT(*) AS event_count,
+      MAX(IF(event_name = 'user_engagement', 1, 0)) AS engaged_session,
+      ${metrics.device_category} AS device,
+      ${metrics.country} AS country
     FROM \`${client.project_id}.${client.source_dataset}.events_*\`
-    WHERE event_name = "session_start"
+    WHERE session_id IS NOT NULL
+    GROUP BY session_id, user_pseudo_id, ${metrics.device_category}, ${metrics.country}
   `);
 }
