40938ed4156d4694c7bf77c120da34aa6f34f122
Hamza atouani <hamza@satidigitalmedia.com>
2025-06-11 05:17:35 +0000

    new client pulled from git

--- includes/clients.js
+++ includes/clients.js
@@ -1,28 +1,27 @@
-module.exports = [
-  {
-    name: "barracuda_phuket",
-    project_id: "sublime-elixir-458201-r4",
-    source_dataset: "analytics_485512127",
-    output_schema: "barracuda_phuket"
-  },
-  {
-    name: "island_escape",
-    project_id: "sublime-elixir-458201-r4",
-    source_dataset: "analytics_427190025",
-    output_schema: "island_escape"
-  },
-  {
-    name: "roost_glamping",
-    project_id: "sublime-elixir-458201-r4",
-    source_dataset: "analytics_399579048",
-    output_schema: "roost_glamping"
-  }
+module.exports = [{
+        name: "barracuda_phuket",
+        project_id: "sublime-elixir-458201-r4",
+        source_dataset: "analytics_485512127",
+        output_schema: "barracuda_phuket"
+    },
+    {
+        name: "island_escape",
+        project_id: "sublime-elixir-458201-r4",
+        source_dataset: "analytics_427190025",
+        output_schema: "island_escape"
+    },
+    {
+        name: "roost_glamping",
+        project_id: "sublime-elixir-458201-r4",
+        source_dataset: "analytics_399579048",
+        output_schema: "roost_glamping"
+    }
 
-{
-  name: "bydlofts_phuket",
-  project_id: "sublime-elixir-458201-r4",
-  source_dataset: "analytics_4081087469",
-  output_schema:  "bydlofts_phuket",
-  google_ads_dataset: "bydlofts_phuket"
-},
+    {
+        name: "bydlofts_phuket",
+        project_id: "sublime-elixir-458201-r4",
+        source_dataset: "analytics_4081087469",
+        output_schema: "bydlofts_phuket",
+        google_ads_dataset: "bydlofts_phuket"
+    },
 ];
