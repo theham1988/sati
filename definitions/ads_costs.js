5d55d49d87429ba4eec03934c2aa63c52adddadb
Hamza atouani <hamza@satidigitalmedia.com>
2025-06-17 09:53:00 +0000

    from

--- definitions/ads_costs.js
+++ definitions/ads_costs.js
@@ -1,3 +1,4 @@
+// definitions/ads_costs.js   – fixed
 const clients = require('includes/clients.js');
 
 for (const client of clients) {
@@ -6,11 +7,11 @@ for (const client of clients) {
     schema: client.output_schema,
   }).query(ctx => `
     SELECT
-      PARSE_DATE('%Y%m%d', _TABLE_SUFFIX)         AS cost_date,
-      metrics.impressions                         AS impressions,
-      metrics.ad_clicks                           AS clicks,
-      metrics.cost_micros / 1e6                   AS ad_cost           -- micros ➜ currency
-    FROM \`${client.project_id}.${client.google_ads_dataset}.Ads_Stats_Date_*\`
+      PARSE_DATE('%Y%m%d', _TABLE_SUFFIX)      AS cost_date,
+      metrics.impressions                      AS impressions,
+      metrics.ad_clicks                        AS clicks,
+      metrics.cost_micros / 1e6               AS ad_cost          -- micros ➜ currency
+    FROM \`${ctx.project}.${client.google_ads_dataset}.Ads_Stats_* \`
     WHERE _TABLE_SUFFIX BETWEEN
           FORMAT_DATE('%Y%m%d', DATE_SUB(CURRENT_DATE(), INTERVAL 60 DAY))
       AND FORMAT_DATE('%Y%m%d', CURRENT_DATE())
